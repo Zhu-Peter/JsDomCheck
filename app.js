38da4efe48e83d21fd961b72383e121fc6859fc2
Zhu-Peter <gzhu@ualberta.ca>
2024-04-04 18:30:10 -0600

    added byelement selectors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let listItem = document.querySelector('li');
 let hello = document.getElementById('hello1');
 let hello2 = document.querySelector('#hello1');
 
-let helloClass = document.querySelectorAll('.hello_world')
+let helloClass = document.querySelectorAll('.hello_world');
+let helloClass2 = document.getElementsByClassName('hello_world');
 
-console.log(helloClass)
+console.log({hello, hello2, helloClass, helloClass2})
