5ca4e60ac0d41b9c4fd4cf54a2ce8cc3bdab006e
Zhu-Peter <gzhu@ualberta.ca>
2024-04-04 18:12:48 -0600

    added content

---
+++ app.js
@@ -0,0 +1,8 @@
+let listItem = document.querySelector('li');
+
+let hello = document.getElementById('hello1');
+let hello2 = document.querySelector('#hello1');
+
+let helloClass = document.querySelectorAll('.hello_world')
+
+console.log(helloClass)
