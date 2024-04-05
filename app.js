38dad03bb29f4fd14c8a29c9db0c8ad694ecb92d
Zhu-Peter <gzhu@ualberta.ca>
2024-04-04 19:08:27 -0600

    added some js manipulation

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let listItem = document.querySelector('li');
+// let listItem = document.querySelector('li');
 
 let hello = document.getElementById('hello1');
-let hello2 = document.querySelector('#hello1');
+// let hello2 = document.querySelector('#hello1');
 
-let helloClass = document.querySelectorAll('.hello_world');
-let helloClass2 = document.getElementsByClassName('hello_world');
+// let helloClass = document.querySelectorAll('.hello_world');
+// let helloClass2 = document.getElementsByClassName('hello_world');
 
-console.log({hello, hello2, helloClass, helloClass2})
+// console.log({hello, hello2, helloClass, helloClass2})
+
+
+hello.style.border = '2px solid orange'
+
+let list = document.querySelectorAll('.hello_world');
+
+for(let i = 0; i < list.length; i++){
+    list[i].innerHTML = 'hacked';
+}
